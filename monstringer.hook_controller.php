8f8371b4545a3249cf1d68104085ccfbf19003f5
Marius Mandal <mariusmandal@gmail.com>
2013-11-08 12:27:04 +0100

    smarter controller

--- monstringer.hook_controller.php
+++ monstringer.hook_controller.php
@@ -1,17 +1,19 @@
 <?php
-
-$monstring = new monstring( get_option('pl_id') );
-
-$monstringer = $monstring->hent_lokalmonstringer();
-$monstringer = array_unique( $monstringer );
-
 $unregistered = 0;
-foreach( $monstringer as $plid ) {
-	$pl = new monstring( $plid );
-	if(!$pl->registered())
-		$unregistered++;
-}
-
 $is_showtime = false;
+
 if( ((int) date('Y') == (int) (get_option('season')-1)) && ((int) date('m') > 10 ) )
 	$is_showtime = true;
+	
+if($is_showtime) {
+	$monstring = new monstring( get_option('pl_id') );
+	
+	$monstringer = $monstring->hent_lokalmonstringer();
+	$monstringer = array_unique( $monstringer );
+	
+	foreach( $monstringer as $plid ) {
+		$pl = new monstring( $plid );
+		if(!$pl->registered())
+			$unregistered++;
+	}	
+}

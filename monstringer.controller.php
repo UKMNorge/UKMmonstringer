8c195299c7305fde38b84316a598b05203be5752
Marius Mandal <mariusmandal@gmail.com>
2013-11-08 11:53:41 +0100

    kontakt-controller

--- monstringer.controller.php
+++ monstringer.controller.php
@@ -11,13 +11,22 @@ $monstringer_data = array();
 foreach( $monstringer as $plid ) {
 	$pl = new monstring( $plid );
 
+	$kontakt_arr = array();
+	$kontakter = $pl->kontakter();
+	for( $kontakter as $k ) {
+		$kontakt_arr = array( 'name'	=> $k->g('name'),
+							  'phone'	=> $k->g('tlf'),
+							  'mail'	=> $k->g('email'),
+							  'picture' => $k->g('picture')
+							 )
+	}
 
 	$monstringer_data[] = array('name' 		=> $pl->g('pl_name'),
 							  'link'		=> $pl->g('link'),
 							  'registered' 	=> $pl->registered(),
 							  'starter'		=> $pl->starter(),
 							  'kommuner'	=> $pl->g('kommuner'),
-							  'kontakter'	=> $pl->kontakter(),
+							  'kontakter'	=> $kontakt_arr,
 							  );
 }
 $INFOS['monstringer'] = $monstringer_data;
